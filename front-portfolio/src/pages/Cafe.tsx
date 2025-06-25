e8b68e94c0fe949d1b11685627ec7d5ea00f2d36
begginersluck <106072320+TU-web-job@users.noreply.github.com>
2025-06-26 00:41:05 +0900

    Add files via upload

---
+++ front-portfolio/src/pages/Cafe.tsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import Header from '../components/cafe/Header';
+import Footer from '../components/cafe/Footer';
+
+const Cafe = () => {
+    return(
+        <>
+    <Header />
+    <Footer />
+    </>
+    );
+};
+
+export default Cafe;
