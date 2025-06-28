429885989ea45ce06f3106d38e5fbd9b0e7ad97c
TU-web-job <t.u.9293991@gmial.com>
2025-06-29 01:40:06 +0900

    detail

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,5 +1,3 @@
-import { url } from "inspector";
-
 export const works = [
     {
         id:"1",
--- src/pages/WorkDetail.tsx
+++ src/pages/WorkDetail.tsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { works } from '../data/works';
+import { Link } from "react-router-dom";
 
 const WorkDetail = () => {
     const { id } = useParams();
@@ -12,7 +13,7 @@ const WorkDetail = () => {
         <section className="py-20 px-4 max-w-4xl mx-auto">
             <h1 className="text-3xl font-bold mb-4">{work.title}</h1>
             <img src={work.image} alt={work.title} className="w-full rounded mb-4" />
-            <a href={work.url} target="_blank" rel="noopener noreferrer" className="inline-block bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600">デモサイトを見る</a>
+            <Link to={work.url} target="_blank" rel="noopener noreferrer" className="inline-block bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600">デモサイトを見る</Link>
             <button onClick={() => navigate(-1)} className="inline-block bg-gray-500 text-white px-6 py-3 rounded hover:bg-gray-600">戻る</button>
         </section>
     );
