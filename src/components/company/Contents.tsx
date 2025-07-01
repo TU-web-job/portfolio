84228e4cfc033afe7cf985bae7eae8b39e6e3be5
TU-web-job <t.u.9293991@gmial.com>
2025-07-01 20:24:18 +0900

    img

--- src/components/company/Contents.tsx
+++ src/components/company/Contents.tsx
@@ -2,20 +2,37 @@ import { content } from '../../data/contents';
 
 const Contents = () => {
     return(
-        <div className="py-20 px-4 text-center items-center">
-            <section>
-                {content.map((item) =>(
-                    <div key={item.id}>
-                        <div className='w-50% items-left'>
-                            <h2 className='font-semibold font-gray-200'>{item.title}</h2>
-                            <p className='text-center'>{item.text}</p>
-                        </div>
-                        <div className='w-50% items-right'>
-                            <img src={item.image}/>
-                        </div>                    
-                    </div>
-                ))}
-            </section>
+        <div className='py-20 px-4'>
+            {content.map((item,index) => {
+                const isEven = index % 2 === 0;
+                return(
+                    <section key={item.id} className={`flex flex-col md:flex-row items-center md:justify-between py-12 ${
+                        isEven ? 'bg-white' : 'bg-gray-100'
+                    }`}>
+                        {isEven ? (
+                            <>
+                            <div className='md:w-1/2 p-4 text-center md:text-left'>
+                                <h2 className='text-2xl font-semibold mb-4'>{item.title}</h2>
+                                <p className='text-gray-700 whitespace-pre-line'>{item.text}</p>
+                            </div>
+                            <div className='md:w-1/2 p-4 flex justify-center'>
+                                <img src={item.image} alt={item.title} className='max-w-full h-auto rounded shadow' />
+                            </div>
+                            </>
+                        ) : (
+                            <>
+                            <div className='md:w-1/2 p-4 text-center md:text-left'>
+                                <h2 className='text-2xl font-semibold mb-4'>{item.title}</h2>
+                                <p className='text-gray-700 whitespace-pre-line'>{item.text}</p>
+                            </div>
+                            <div className='md:w-1/2 p-4 flex justify-center'>
+                                <img src={item.image} alt={item.title} className='max-w-full h-auto rounded shadow' />
+                            </div>
+                            </>
+                        )}
+                    </section>
+                );
+            })}
         </div>
     );
 };
