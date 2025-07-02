78048db70a6e65e2dd119dae1e91a267dd0683e6
TU-web-job <t.u.9293991@gmial.com>
2025-07-02 20:03:19 +0900

    footer

--- src/components/company/Footer.tsx
+++ src/components/company/Footer.tsx
@@ -2,14 +2,14 @@ import { footerList } from "../../data/contents";
 
 const Footer = () => {
     return(
-        <footer>
+        <footer className="bg-gray-100">
             <div><h3 className="font-bold text-navy-100">Company</h3></div>
             <div>
                 {footerList.map((list) => {
                     return(
                     <>
                     <ul>
-                        <li key={list.id} className="style-none">{list.name}</li>
+                        <li key={list.id} className="style-none"><a className="border-bottom">{list.name}</a></li>
                     </ul>
                     </>
                     );
